d1af5acdff4ca2af0c2b8f15b006124d357abb95
nicovarLI <nicolas.varela@lightit.io>
2023-07-13 09:11:22 -0300

    refactor: Deleted commented code

--- src/GetMovieInfoCommand.php
+++ src/GetMovieInfoCommand.php
@@ -30,72 +30,21 @@ class GetMovieInfoCommand extends Command{
 
     private function render(OutputInterface $output,$request){
         $response = json_decode($request->getBody(),true);
-        $arr = array();
         $aux = array();
-        //$info = array_values($response);
         foreach ($response as $row){
-            //var_dump($data);
-            //$aux[] = $row;
             if(is_array($row)){
                $str = implode(",",$row);
                array_push($aux,$str);
             }else{
             array_push($aux,$row);
             }
-            //$cont+=1;
-            //var_dump($cont);
-            //$aux = array($data);
-            //array_push($arr,$aux);
-            //aux no esta siendo array, ver eso, valor displayed
         }
-
-        $arr[0]= $aux;
-        //var_dump($arr);
-        //var_dump($arr);
         $titles = array_keys($response);
         $table = new Table($output);
         $table->setHeaders($titles)
               ->setHorizontal()
               ->addRow($aux);
               $table->render();
-        //foreach($aux as $row){
-        //        $arr[]= $row;
-        //        var_dump($row);
-        //        var_dump($arr);
-                //$table->addRow($arr);
-        //}
-       // foreach ($response as $row){
-         //   if(!is_array($row)){
-           //     $rowData = explode(',',$row);
-             //   $table->addRow($rowData);
-            //}else{
-              //  $table->addRow($row);
-            //}
-        //}
-        
-        //$rows = array_map(function ($item) {
-        //    return is_array($item) ? array_values($item) : [$item];
-        //}, $response);
-
-        // Add the rows to the table
-        //foreach ($rows as $row) {
-        //    $table->addRow($row);
-        //}
-        
-        
-        //$rows = [];
-        //foreach ($response as $row) {
-            //if(is_string($row)){
-            //    $rowData = explode(',',$row);
-          //  }else{
-        //        $rowData = $row;
-        //    }
-        //    $rows[] = array_values($rowData);
-        //}
-        //$table->setRows($rows);
-        
-              //->addRows($arr)
-        
         
     }
 }
